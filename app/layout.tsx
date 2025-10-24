d6490e5a68f422f925d54060dead4427adb96ffc
edenbd1 <eden.baudin.invest@gmail.com>
2025-10-24 14:37:20 +0200

    rebranding

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export async function generateMetadata(): Promise<Metadata> {
         version: minikitConfig.miniapp.version,
         imageUrl: minikitConfig.miniapp.heroImageUrl,
         button: {
-          title: `Join the ${minikitConfig.miniapp.name} Waitlist`,
+          title: `Start Watching`,
           action: {
             name: `Launch ${minikitConfig.miniapp.name}`,
             type: "launch_frame",
